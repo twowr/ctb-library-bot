f25afd10401977f12bdfea8d1a044f72330cef3a
2wr <taolaolgc@gmail.com>
2022-08-03 15:39:05 +0700

    remove unneccessary line

--- push_commands.js
+++ push_commands.js
@@ -2,7 +2,6 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
 
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
