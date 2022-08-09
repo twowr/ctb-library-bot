c7eb10eedf969434b2a1095a6a2c25cc06074c6b
2wr <taolaolgc@gmail.com>
2022-08-09 20:30:35 +0700

    fix setactivity error

--- events/ready.js
+++ events/ready.js
@@ -6,6 +6,8 @@ module.exports = {
     async execute(client) {
         History.sync()
         Player.sync()
+
+        client.user.setActivity('is catching the beat')
         console.log(`Logged in as ${client.user.tag}!`)
     }
 }
