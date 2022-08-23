183c72b50d7bf6c374514ecd2fedfbceaeb585f0
Med367367 <45147847+Med367367@users.noreply.github.com>
2022-08-23 02:53:43 -0400

    Custom Activity
    Bad performances line reading code. Would need a fix if the .txt file become big.

--- events/ready.js
+++ events/ready.js
@@ -2,13 +2,29 @@ const { ActivityType } = require('discord.js')
 
 const { Player, PlayerEvent } = require('../database.js')
 
+const fs = require("fs");
+const statusPath = "status.txt"
+
+function getRandomLine(filePath){ // Bad Performances with big .txt files, would need a fix later.
+    var data = fs.readFileSync(filePath, 'utf8')
+
+    const lines = data.split(/\r?\n/) 
+    const randomLineNumber = Math.floor(Math.random() * lines.length)
+    return lines[randomLineNumber]
+}
+
 module.exports = {
     name: 'ready',
     async execute(client) {
+        console.log(`Logged in as ${client.user.tag}!`)
         PlayerEvent.sync()
         Player.sync()
 
-        client.user.setActivity('黒皇帝 - Scattered Faith', { type: ActivityType.Listening })
-        console.log(`Logged in as ${client.user.tag}!`)
+        client.user.setActivity(getRandomLine(statusPath), { type: ActivityType.Listening })
+
+        var minutes = 60, intervalTimer = minutes * 60 * 1000;
+        setInterval(function() {
+            client.user.setActivity(getRandomLine(statusPath), { type: ActivityType.Listening })
+        }, intervalTimer);
     }
 }
