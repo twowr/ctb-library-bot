70cee056594ca046d20275622c7057c86d6ba123
2wr <taolaolgc@gmail.com>
2022-08-23 12:29:49 +0700

    forgot to delete experiment

--- database.js
+++ database.js
@@ -9,6 +9,6 @@ const sequelize = new Sequelize('database', 'username', 'password', {
 const Player = require('./models/player')(sequelize, Sequelize)
 const PlayerEvent = require('./models/playerEvent')(sequelize, Sequelize)
 
-PlayerEvent.belongsTo(Player, { as: 'Player' })
+PlayerEvent.belongsTo(Player)
 
 module.exports = { Player, PlayerEvent }
